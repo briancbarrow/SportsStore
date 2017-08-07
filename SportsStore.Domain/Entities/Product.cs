6715a62939e06e10216d2c4dd364b300d57444ba
Brian Barrow <briancbarrow@gmail.com>
2017-08-07 14:09:08 -0600

    Save functionality added

--- SportsStore.Domain/Entities/Product.cs
+++ SportsStore.Domain/Entities/Product.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 using System.Threading.Tasks;
 
 namespace SportsStore.Domain.Entities
 {
     public class Product
     {
+        [HiddenInput(DisplayValue = false)]
         public int ProductID { get; set; }
         public string Name { get; set; }
+
+        [DataType(DataType.MultilineText)]
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Category { get; set; }
